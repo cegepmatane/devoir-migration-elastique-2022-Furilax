fd56ab49526cce10e297fd6c27cea117f91ba245
Matteo De Lutiis <delutiismatteo@gmail.com>
2022-03-07 12:57:09 -0500

    Ajout du fichier ajouter en php

---
+++ app-rds/ajouter.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+require_once "Ecouteur.php";
+require_once "EcouteurDAO.php";
+
+$ecouteurJSON = file_get_contents('php://input');
+$ecouteurObjet = json_decode( $ecouteurJSON );
+$ecouteur = new Ecouteur($ecouteurObjet);
+
+$id = EcouteurDAO::ajouter($ecouteur);
+echo $id;
